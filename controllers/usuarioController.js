dc2d8825ec5214dab85086883fdadf0f09b51573
Alberto <albertopimentel04@gmail.com>
2022-04-19 13:31:00 -0500

    Generar id para token unico

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/Usuario.js";
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req, res) => {
   //Evitar registros duplicados
@@ -11,8 +12,10 @@ const registrar = async (req, res) => {
       msg: error.message,
     });
   }
+
   try {
     const usuario = new Usuario(req.body);
+    usuario.token = generarId();
     const usuarioAlmacenado = await usuario.save();
     res.json(usuarioAlmacenado);
   } catch (error) {
