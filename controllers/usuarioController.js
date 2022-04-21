4568e630205730fd0300c3425e37351d7f832a4b
Alberto <albertopimentel04@gmail.com>
2022-04-21 16:24:24 -0500

    Configuracion del cors

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,8 +17,10 @@ const registrar = async (req, res) => {
   try {
     const usuario = new Usuario(req.body);
     usuario.token = generarId();
-    const usuarioAlmacenado = await usuario.save();
-    res.json(usuarioAlmacenado);
+    await usuario.save();
+    res.json({
+      msg: "Usuario creado Correctamente, Revisa tu Email para confirmar tu cuenta",
+    });
   } catch (error) {
     console.log(`(╯°□°）╯︵ ┻━┻ |>ERROR: ${error}`);
   }
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import proyectoRoutes from "./routes/proyectoRoutes.js";
 import tareaRoutes from "./routes/tareaRoutes.js";
-
 const app = express();
 app.use(express.json());
 
 dotenv.config();
 
 conectarDB();
+
+//Configuracion de CORS
+const whiteList = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      //Puede consultar la API
+      callback(null, true);
+    } else {
+      //No puede consultar la API
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 //Routing
 app.use("/api/usuarios", usuarioRoutes);
